452047e85bb36609061da732cf6a67bfc1ba103f
joakim <mtuko2@gmail.com>
2025-12-12 16:57:42 +0300

    weather

--- frontend/src/app/components/home/home.ts
+++ frontend/src/app/components/home/home.ts
@@ -16,7 +16,30 @@ export class HomeComponent implements OnInit {
   constructor(private api: ApiService) { }
 
   ngOnInit() {
-    // Determine location or use default (Nairobi)
+    this.getUserLocation();
+  }
+
+  getUserLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.api.getWeather(position.coords.latitude, position.coords.longitude).subscribe(res => {
+            this.weather = res;
+          });
+        },
+        (error) => {
+          console.warn('Location permission denied or unavailable, using default location.');
+          this.getWeatherDefault();
+        }
+      );
+    } else {
+      console.warn('Geolocation is not supported by this browser.');
+      this.getWeatherDefault();
+    }
+  }
+
+  getWeatherDefault() {
+    // Default to Nairobi
     this.api.getWeather(-1.2921, 36.8219).subscribe(res => {
       this.weather = res;
     });
